d793993b46d954c09abe7f7244056f7d4aa616d9
DiscordBotMaker <108666318+TimothyGitDev@users.noreply.github.com>
2024-08-14 21:35:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 let money = document.getElementById('money');
 let btn = document.getElementById('btn');
 
+let Upgradebtn = document.getElementById('UpgradeMoney');
+let price = document.getElementById('price');
+
+let clickMoney = 1;
 let counter = 0;
-btn.addEventListener('click', function() {
-    counter++;
-    money.textContent = counter + '$';
+let upgradeCost = 10;
+
+price.textContent = upgradeCost;
+
+btn.addEventListener('click', () => {
+    counter += clickMoney;
+    money.textContent = counter + '$'; 
+}); 
+
+Upgradebtn.addEventListener('click', () => {
+
+    if(counter >= upgradeCost) {
+        counter = Math.floor(counter - upgradeCost);
+
+        clickMoney *= 2;
+        upgradeCost *= 3;
+        Upgradebtn.textContent = 'Купить улучшение ' + upgradeCost; 
+        money.textContent = counter + '$';    
+    }
 });
+
+
+
